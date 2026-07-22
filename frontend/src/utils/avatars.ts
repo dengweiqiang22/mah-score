9014b548a5beaf35a74c25c6f9c35fe1e08f9779
dengweiqiang <dengweiqiang66@163.com>
2026-07-22 15:20:58 +0900

    feat(frontend): 替换头像资源为裁切图标
    - 从预览图裁切生成 16 个英文文件名头像 SVG
    
    - 更新头像 registry 使用新头像并加入猫头鹰和小鸡
    
    - 删除旧中文文件名头像资源

--- frontend/src/utils/avatars.ts
+++ frontend/src/utils/avatars.ts
@@ -1,19 +1,19 @@
-import bearUrl from "../assets/avatars/棕熊.svg";
-import catUrl from "../assets/avatars/猫咪.svg";
-import cowUrl from "../assets/avatars/奶牛.svg";
-import dogUrl from "../assets/avatars/小狗.svg";
-import foxUrl from "../assets/avatars/狐狸.svg";
-import frogUrl from "../assets/avatars/青蛙.svg";
-import hamsterUrl from "../assets/avatars/仓鼠.svg";
-import koalaUrl from "../assets/avatars/考拉.svg";
-import lionUrl from "../assets/avatars/狮子.svg";
-import monkeyUrl from "../assets/avatars/猴子.svg";
-import pandaUrl from "../assets/avatars/熊猫.svg";
-import penguinUrl from "../assets/avatars/企鹅.svg";
-import pigUrl from "../assets/avatars/小猪.svg";
-import rabbitUrl from "../assets/avatars/兔子.svg";
-import raccoonUrl from "../assets/avatars/浣熊.svg";
-import tigerUrl from "../assets/avatars/老虎.svg";
+import catUrl from "../assets/avatars/cat.svg";
+import chickUrl from "../assets/avatars/chick.svg";
+import cowUrl from "../assets/avatars/cow.svg";
+import dogUrl from "../assets/avatars/dog.svg";
+import foxUrl from "../assets/avatars/fox.svg";
+import frogUrl from "../assets/avatars/frog.svg";
+import hamsterUrl from "../assets/avatars/hamster.svg";
+import lionUrl from "../assets/avatars/lion.svg";
+import monkeyUrl from "../assets/avatars/monkey.svg";
+import owlUrl from "../assets/avatars/owl.svg";
+import pandaUrl from "../assets/avatars/panda.svg";
+import penguinUrl from "../assets/avatars/penguin.svg";
+import pigUrl from "../assets/avatars/pig.svg";
+import rabbitUrl from "../assets/avatars/rabbit.svg";
+import raccoonUrl from "../assets/avatars/raccoon.svg";
+import tigerUrl from "../assets/avatars/tiger.svg";
 
 export interface AvatarOption {
   readonly id: string;
@@ -23,21 +23,21 @@ export interface AvatarOption {
 
 export const avatarOptions: readonly AvatarOption[] = [
   { id: "panda", label: "熊猫", src: pandaUrl },
-  { id: "tiger", label: "老虎", src: tigerUrl },
+  { id: "fox", label: "狐狸", src: foxUrl },
   { id: "cat", label: "猫咪", src: catUrl },
+  { id: "dog", label: "小狗", src: dogUrl },
+  { id: "rabbit", label: "兔子", src: rabbitUrl },
   { id: "frog", label: "青蛙", src: frogUrl },
-  { id: "monkey", label: "猴子", src: monkeyUrl },
+  { id: "penguin", label: "企鹅", src: penguinUrl },
+  { id: "lion", label: "狮子", src: lionUrl },
+  { id: "tiger", label: "老虎", src: tigerUrl },
+  { id: "raccoon", label: "浣熊", src: raccoonUrl },
   { id: "hamster", label: "仓鼠", src: hamsterUrl },
   { id: "pig", label: "小猪", src: pigUrl },
-  { id: "rabbit", label: "兔子", src: rabbitUrl },
-  { id: "bear", label: "棕熊", src: bearUrl },
-  { id: "dog", label: "小狗", src: dogUrl },
-  { id: "raccoon", label: "浣熊", src: raccoonUrl },
-  { id: "lion", label: "狮子", src: lionUrl },
-  { id: "fox", label: "狐狸", src: foxUrl },
   { id: "cow", label: "奶牛", src: cowUrl },
-  { id: "penguin", label: "企鹅", src: penguinUrl },
-  { id: "koala", label: "考拉", src: koalaUrl },
+  { id: "monkey", label: "猴子", src: monkeyUrl },
+  { id: "owl", label: "猫头鹰", src: owlUrl },
+  { id: "chick", label: "小鸡", src: chickUrl },
 ];
 
 export const defaultAvatarId = avatarOptions[0]?.id ?? "panda";
